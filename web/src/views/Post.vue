ab2490ef6a32e0965802fc676554869e98399462
Jianqi Pan <jannchie@gmail.com>
2025-02-23 01:13:56 +0900

    feat(post-view): add back button && update layout with scroll area

--- web/src/views/Post.vue
+++ web/src/views/Post.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { selectedPostIdSet } from '@/shared'
 import { getPostImageURL } from '@/utils'
+import { Btn } from '@roku-ui/vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
@@ -11,23 +12,46 @@ const scrollAreaRef = ref<HTMLElement>()
 </script>
 
 <template>
-  <ScrollArea
+  <div
     v-if="post"
-    ref="scrollAreaRef"
-    class="relative h-full w-full flex flex-col items-center gap-4"
+    class="h-full flex flex-col"
   >
-    <div class="h-80% w-fit">
-      <img
-        :src="getPostImageURL(post)"
-        alt="post"
-        class="h-full w-full overflow-hidden rounded-lg object-contain"
-        @click=" selectedPostIdSet = new Set([post.id])"
-      >
+    <div
+      class="flex items-center justify-between border-b bg-surface-base"
+    >
+      <div class="grow-1 basis-0">
+        <Btn
+          icon
+          variant="transparent"
+          color="surface"
+          @click="$router.back()"
+        >
+          <i class="i-tabler-arrow-left" />
+        </Btn>
+      </div>
+      <span class="text-sm">
+        {{ post.file_name }}
+      </span>
+      <span class="grow-1 basis-0" />
     </div>
-    <SimilarPosts
-      v-if="scrollAreaRef"
-      :post-id="post.id"
-      :scroll-element="scrollAreaRef"
-    />
-  </ScrollArea>
+    <ScrollArea
+      ref="scrollAreaRef"
+      class="relative h-full w-full flex flex-grow flex-basis-0 flex-col items-center gap-4"
+    >
+      <div class="h-80% w-fit px-2">
+        <img
+          :key="post.id"
+          :src="getPostImageURL(post)"
+          alt="post"
+          class="h-full w-full overflow-hidden rounded-lg object-contain"
+          @click=" selectedPostIdSet = new Set([post.id])"
+        >
+      </div>
+      <SimilarPosts
+        v-if="scrollAreaRef"
+        :post-id="post.id"
+        :scroll-element="scrollAreaRef"
+      />
+    </ScrollArea>
+  </div>
 </template>
