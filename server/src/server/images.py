ea03ea7d8ccc94ea5d4da780983d25db2d13cb61
Jianqi Pan <jannchie@gmail.com>
2025-04-16 22:35:22 +0900

    feat(images): add image controller for original and thumbnails

---
+++ server/src/server/images.py
@@ -0,0 +1,45 @@
+import mimetypes
+from typing import ClassVar
+
+from litestar import Controller, get
+from litestar.exceptions import HTTPException
+from litestar.response import File
+
+import shared
+from utils import create_thumbnail
+
+mimetypes.add_type("image/jpeg", ".jpg")
+mimetypes.add_type("image/jpeg", ".jpeg")
+mimetypes.add_type("image/png", ".png")
+mimetypes.add_type("image/webp", ".webp")
+mimetypes.add_type("image/gif", ".gif")
+mimetypes.add_type("image/bmp", ".bmp")
+mimetypes.add_type("image/tiff", ".tiff")
+mimetypes.add_type("image/svg+xml", ".svg")
+mimetypes.add_type("image/vnd.microsoft.icon", ".ico")
+mimetypes.add_type("image/heic", ".heic")
+mimetypes.add_type("image/heif", ".heif")
+mimetypes.add_type("image/avif", ".avif")
+mimetypes.add_type("image/x-icon", ".ico")
+
+
+class ImageController(Controller):
+    path = "/images"
+    tags: ClassVar[list[str]] = ["images"]
+
+    @get("/original/{post_path:path}")
+    async def get_original(self, post_path: str) -> File:
+        abs_path = shared.target_dir / post_path[post_path.rfind("/") + 1 :]
+        if not abs_path.exists():
+            raise HTTPException(status_code=404, detail="File not found")
+        media_type, _ = mimetypes.guess_type(abs_path)
+        return File(abs_path, media_type=media_type)
+
+    @get("/thumbnails/{post_path:path}", tags=["Image"])
+    async def get_thumbnail(self, post_path: str) -> File:
+        thumbnail_file_path = shared.thumbnails_dir / post_path[post_path.rfind("/") + 1 :]
+        original_file_path = shared.target_dir / post_path[post_path.rfind("/") + 1 :]
+        if not thumbnail_file_path.exists():
+            create_thumbnail(original_file_path, thumbnail_file_path)
+        media_type, _ = mimetypes.guess_type(thumbnail_file_path)
+        return File(thumbnail_file_path, media_type=media_type)
