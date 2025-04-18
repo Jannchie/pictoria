7daf864d151895ce0a1ef733a4e61b8714da1246
Jianqi Pan <jannchie@gmail.com>
2025-04-18 20:36:52 +0900

    refactor(images): use NotFoundException instead of HTTPException && update tags casing

--- server/src/server/images.py
+++ server/src/server/images.py
@@ -2,7 +2,7 @@ import mimetypes
 from typing import ClassVar
 
 from litestar import Controller, get
-from litestar.exceptions import HTTPException
+from litestar.exceptions import NotFoundException
 from litestar.response import File
 
 import shared
@@ -31,7 +31,7 @@ class ImageController(Controller):
     """
 
     path = "/images"
-    tags: ClassVar[list[str]] = ["images"]
+    tags: ClassVar[list[str]] = ["Images"]
 
     @get("/original/{post_path:path}")
     async def get_original(self, post_path: str) -> File:
@@ -40,7 +40,7 @@ class ImageController(Controller):
         """
         abs_path = shared.target_dir / post_path[post_path.rfind("/") + 1 :]
         if not abs_path.exists():
-            raise HTTPException(status_code=404, detail="Original image not found")
+            raise NotFoundException(detail="Original image not found")
         media_type, _ = mimetypes.guess_type(abs_path)
         return File(abs_path, media_type=media_type)
 
@@ -54,7 +54,7 @@ class ImageController(Controller):
             thumbnail_file_path.parent.mkdir(parents=True, exist_ok=True)
         original_file_path = shared.target_dir / post_path[post_path.rfind("/") + 1 :]
         if not original_file_path.exists():
-            raise HTTPException(status_code=404, detail="Original image not found")
+            raise NotFoundException(detail="Original image not found")
         if not thumbnail_file_path.exists():
             create_thumbnail(original_file_path, thumbnail_file_path)
         media_type, _ = mimetypes.guess_type(thumbnail_file_path)
