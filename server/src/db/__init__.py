776fc3d69effc54d689312cbd052d786a8f08be1
Jianqi Pan <jannchie@gmail.com>
2025-04-22 23:38:27 +0900

    feat(db): add async db connection && load environment variables

--- server/src/db/__init__.py
+++ server/src/db/__init__.py
@@ -1,12 +1,17 @@
+import os
 from pathlib import Path
 
 import numpy as np
+from dotenv import load_dotenv
 from pydantic import BaseModel
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from ai.clip import calculate_image_features
 from models import Post, PostVector
 from utils import get_session
+
+load_dotenv()
 
 
 def insert_img_vec(post_id: int, image_path: Path):
@@ -40,3 +45,8 @@ def find_similar_posts(vec: np.ndarray, *, limit: int = 100) -> list[SimilarImag
         query = select(PostVector.post_id, distance.label("distance")).order_by(distance).limit(limit).offset(1)
         result = session.execute(query).all()
     return [SimilarImageResult(post_id=row[0], distance=row[1]) for row in result]
+
+
+db_url = os.environ.get("DB_URL")
+aengine = create_async_engine(db_url, echo=False, pool_size=100, max_overflow=200)
+ASession = async_sessionmaker(bind=aengine, expire_on_commit=False)
